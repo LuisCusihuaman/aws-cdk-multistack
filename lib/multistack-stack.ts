c4cb95b5c2d12d8033b76314291e84d83c68fce9
bshawnd <shawnblanchard@gmail.com>
2021-01-18 19:53:02 -0600

    initial commit

--- lib/multistack-stack.ts
+++ lib/multistack-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3'; 
 
 export class MultistackStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
